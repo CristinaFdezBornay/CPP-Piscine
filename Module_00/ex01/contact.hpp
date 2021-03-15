98972161fb8ce6fb8f1cf6647dc40238d01034c2
cristina <cristina@42network.org>
2021-03-15 19:20:35 +0100

    feat(cpp00 ex01): generally working execpt print

--- Module_00/ex01/contact.hpp
+++ Module_00/ex01/contact.hpp
@@ -3,6 +3,7 @@
 class       Contact{
     public:
         int         Create(int id);
+        int         print_general_information();
     private:
         int         identifier;
         std::string first_name;
@@ -16,9 +17,72 @@ class       Contact{
         std::string fav_meal;
         std::string underwear_color;
         std::string darkest_secret;
+        std::string format_information(std::string information);
 };
 
 int             Contact::Create(int id){
     identifier = id;
+    std::cout << "Introduce your first name:" << std::endl;
+    std::cin >> first_name;
+    std::cout << "Introduce your last name:" << std::endl;
+    std::cin >> last_name;
+    std::cout << "Introduce your nickname:" << std::endl;
+    std::cin >> nickname;
+    // std::cout << "Introduce your login:" << std::endl;
+    // std::cin >> login;
+    // std::cout << "Introduce your postal address:" << std::endl;
+    // std::cin >> postal_adress;
+    // std::cout << "Introduce your email address:" << std::endl;
+    // std::cin >> email_adress;
+    // std::cout << "Introduce your phone number:" << std::endl;
+    // std::cin >> phone_number;
+    // std::cout << "Introduce your birthday date:" << std::endl;
+    // std::cin >> birthday_date;
+    // std::cout << "Introduce your favourite meal:" << std::endl;
+    // std::cin >> fav_meal;
+    // std::cout << "Introduce your underwear color:" << std::endl;
+    // std::cin >> underwear_color;
+    // std::cout << "Introduce your darkest secret:" << std::endl;
+    // std::cin >> darkest_secret;
+    return 0;
+}
+
+std::string     Contact::format_information(std::string information) {
+    int         len;
+    std::string formatted_information;
+
+    len = int(information.length());
+    formatted_information = "";
+    if (len == 10)
+        return information;
+    if (len > 10) {
+        for (int i = 0; i < 10; ++i)
+            formatted_information[i] = information[i];
+        formatted_information[10] = '.';
+        return formatted_information;
+    }
+    for (int i = 0; i < 10 - len; ++i)
+        formatted_information[i] = ' ';
+    // for (int j = 0; j < 10; ++j) {
+    //     formatted_information[len + j] += information[j];
+    // }
+    std::cout << formatted_information << std::endl;
+    formatted_information[10 - len] += information;
+    return formatted_information;
+}
+
+int              Contact::print_general_information() {
+    std::string general_information;
+
+    general_information = "| ";
+    general_information += identifier + '0';
+    general_information += " |";
+    general_information += format_information(first_name);
+    general_information += "|";
+    general_information += format_information(last_name);
+    general_information += "|";
+    general_information += format_information(nickname);
+    general_information += "|";
+    std::cout << general_information << std::endl;
     return 0;
 }
--- Module_00/ex01/main.cpp
+++ Module_00/ex01/main.cpp
@@ -1,37 +1,43 @@
 #include "./contact.hpp"
 
+int         program(Contact *phonebooooook, int contacts);
+
 int         add_contact(Contact *phonebooooook, int contacts) {
-    (void)phonebooooook;
+    if (contacts > 8)
+        std::cout << "The phonebooooook is full, you cannot ADD." << std::endl;
+    else
+        phonebooooook[contacts].Create(contacts);
     contacts += 1;
-    std::cout << "lol" << std::endl;
-    return contacts;
+    return program(phonebooooook, contacts);
 }
 
-void        search_contact(Contact *phonebooooook, int contacts) {
-    (void)phonebooooook;
-    std::cout << contacts << std::endl;
+int        search_contact(Contact *phonebooooook, int contacts) {
+    for (int i = 0; i < contacts; ++i)
+        phonebooooook[i].print_general_information();
+    return program(phonebooooook, contacts);
 }
 
-int         program(Contact *phonebooooook) {
-    int             contacts;
+int         program(Contact *phonebooooook, int contacts) {
     std::string     command;
 
-    contacts = 0;
     std::cout << "Enter a command [ADD, SEARCH, EXIT]:" << std::endl;
     std::cin >> command;
-    if (command == "ADD") {
-        contacts = add_contact(phonebooooook, contacts);
-    }
-    else if (command == "SEARCH") {
-        search_contact(phonebooooook, contacts);
-    }
-    return 0; 
+    if (command == "ADD" or command == "1")
+        return add_contact(phonebooooook, contacts);
+    else if (command == "SEARCH" or command == "2")
+        return search_contact(phonebooooook, contacts);
+    else if (command == "EXIT" or command == "3")
+        return 0;
+    return 1;
 }
 
 int         main(void) {
+    int             output;
     Contact         *phonebooooook;
 
     phonebooooook = new Contact[8];
-    program(phonebooooook);
-    return 0;
+    output = program(phonebooooook, 0);
+    if (output == 1)
+        std::cout << "There has been a problem while processing!" << std::endl;
+    return output;
 }
