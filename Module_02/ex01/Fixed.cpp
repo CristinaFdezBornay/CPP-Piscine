46cfd772b3e18de448d4152c1db57d6fa6359299
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2021-03-31 18:17:33 +0200

    feat(cpp02 ex01): working

---
+++ Module_02/ex01/Fixed.cpp
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crfernan <crfernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/31 15:30:20 by crfernan          #+#    #+#             */
+/*   Updated: 2021/03/31 18:12:52 by crfernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed( void ) : _fpv( 0 ) {
+    std::cout << "Default constructor called" << std::endl;
+    return ;
+}
+
+Fixed::~Fixed( void ) {
+    std::cout << "Destructor called" << std::endl;
+    return ;
+}
+
+Fixed::Fixed( const int nbr ) {
+    std::cout << "Int constructor called" << std::endl;
+    this->setRawBits( ( nbr << Fixed::_nbrFractBits ) );
+    return ;
+}
+
+Fixed::Fixed( const float nbr ) : _fpv( int(nbr) ) {
+    std::cout << "Float constructor called" << std::endl;
+    this->setRawBits( roundf( nbr * ( 1 << Fixed::_nbrFractBits ) ) );
+    return ;
+}
+
+Fixed::Fixed( Fixed const & src ) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = src;
+    return ;
+}
+
+int                 Fixed::getRawBits( void ) const {
+    return this->_fpv;
+}
+
+void                Fixed::setRawBits( int const raw ) {
+    this->_fpv = raw;
+    return ;
+}
+
+float               Fixed::toFloat( void )const {
+    return ((float)this->getRawBits() / (float)(1 << Fixed::_nbrFractBits));
+}
+
+int                 Fixed::toInt( void )const {
+    return (this->getRawBits() >> Fixed::_nbrFractBits);
+}
+
+Fixed &             Fixed::operator=( Fixed const & rhs ) {
+    std::cout << "Assignation operator called" << std::endl;
+    if ( this != &rhs )
+        this->setRawBits( rhs.getRawBits() );
+    return *this;
+}
+
+std::ostream &     operator<<( std::ostream & o, Fixed const & i ) {
+    o << i.toFloat() ;
+    return o;
+}
--- Module_02/ex01/Fixed.hpp
+++ Module_02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: crfernan <crfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 15:30:18 by crfernan          #+#    #+#             */
-/*   Updated: 2021/03/31 17:59:15 by crfernan         ###   ########.fr       */
+/*   Updated: 2021/03/31 18:10:41 by crfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,14 +29,14 @@ class       Fixed {
         int                 getRawBits( void ) const;
         void                setRawBits( int const raw );
 
-        float               tofloat( void )const;
         int                 toInt( void )const;
+        float               toFloat( void )const;
 
     private:
         int                 _fpv;
-        static const int    _nbrFractBits;
+        static const int    _nbrFractBits = 8;
 };
 
-std::ostream &     operator<<( std::ostream & o, Fixed const & i );
+std::ostream &     operator<<( std::ostream & o, Fixed const & src );
 
 #endif
