d5c290ad84f867e5d9e95e09a5b6bb817d5888df
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2021-04-18 22:57:27 +0200

    feat(cpp04 ex01): main to test

--- Module_04/ex01/main.cpp
+++ Module_04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: crfernan <crfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/09 00:54:22 by crfernan          #+#    #+#             */
-/*   Updated: 2021/04/12 17:10:04 by crfernan         ###   ########.fr       */
+/*   Updated: 2021/04/18 22:56:16 by crfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,22 @@
 #include "PowerFist.hpp"
 #include "RadScorpion.hpp"
 
-int main()
-{
+void    test( void ) {
+    std::cout << "LOL" << std::endl;
+}
+
+void    check_args_and_run_test( int argc, char **argv ) {
+    if ( argc == 1 or argv[1][0] != 't' )
+        return ;
+    std::cout << std::endl;
+    std::cout << "============================= EXTRA TESTS =============================" << std::endl;
+    test( );
+    std::cout << "=======================================================================" << std::endl;
+    std::cout << std::endl;
+}
+
+
+int     main( int argc, char **argv ) {
     Character*  me = new Character("me");
     std::cout << *me;
 
@@ -38,6 +52,8 @@ int main()
     std::cout << *me;
     me->attack(b);
     std::cout << *me;
+
+    check_args_and_run_test( argc, argv );
 
     return 0;
 }
