9e0664076dc3be6b600240997c2567f5ff481009
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2021-04-29 00:30:27 +0200

    fix(cpp04 ex02): lint

--- Module_04/ex02/TacticalMarine.hpp
+++ Module_04/ex02/TacticalMarine.hpp
@@ -6,7 +6,7 @@
 /*   By: crfernan <crfernan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/22 18:18:06 by crfernan          #+#    #+#             */
-/*   Updated: 2021/04/26 17:52:47 by crfernan         ###   ########.fr       */
+/*   Updated: 2021/04/29 00:22:20 by crfernan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ class  TacticalMarine : public ISpaceMarine {
     public:
         TacticalMarine( void );
         ~TacticalMarine( void );
-        TacticalMarine(  TacticalMarine const & rhs );
+        TacticalMarine( TacticalMarine const & rhs );
 
-        TacticalMarine*      clone( void ) const;
+        TacticalMarine*         clone( void ) const;
         void                    battleCry( void ) const;
         void                    rangedAttack( void ) const;
         void                    meleeAttack( void ) const;
 
-        TacticalMarine &     operator=( TacticalMarine const & rhs );
+        TacticalMarine &        operator=( TacticalMarine const & rhs );
 };
 
 #endif
