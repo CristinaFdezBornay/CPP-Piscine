41a7cd8d8e0e01119ca4a8b46df3089c547ad5d6
Cristina Fernández Bornay <cristinafernandezbornay@outlook.com>
2021-09-24 12:25:36 +0200

    feat(cpp06 ex01): working

---
+++ Module_06/ex01/Data.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crfernan <crfernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/24 12:00:11 by crfernan          #+#    #+#             */
+/*   Updated: 2021/09/24 12:04:39 by crfernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DATA_HPP
+# define DATA_HPP
+
+# include <iostream>
+
+struct              Data {
+    std::string     d_str1;
+    int             d_int;
+    std::string     d_str2;
+}                   data;
+
+#endif
---
+++ Module_06/ex01/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crfernan <crfernan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/24 12:00:02 by crfernan          #+#    #+#             */
+/*   Updated: 2021/09/24 12:25:04 by crfernan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Data.hpp"
+
+uintptr_t   serialize( Data* ptr ) {
+    return (uintptr_t)ptr;
+}
+
+Data *      deserialize( uintptr_t raw ) {
+    return reinterpret_cast<Data *>(raw);
+}
+
+int         main( void ) {
+    Data        *data = new Data;
+    Data        *ptr = NULL;
+    uintptr_t   serialized_data;
+
+    std::cout << "Data address    : " << data << std::endl;
+    data->d_str1 = "toto";
+    data->d_int = 42;
+    data->d_str2 = "tata";
+
+    serialized_data = serialize(data);
+    std::cout << "Serialized data : " << serialized_data << std::endl;
+
+    ptr = deserialize(serialized_data);
+    std::cout << "Recovered data  : " << ptr << std::endl << std::endl;
+    std::cout << "ptr->d_str1  => "<< ptr->d_str1 << std::endl;
+    std::cout << "ptr->d_int   => "<< ptr->d_int << std::endl;
+    std::cout << "ptr->d_str2  => "<< ptr->d_str2 << std::endl;
+
+    return 0;
+}
